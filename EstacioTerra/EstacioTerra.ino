8c355209edb9f5b6a43704dc80794b6655407134
LuciaRodriguezCanals <lucia.rodriguez.canals@estudiantat.upc.edu>
2025-10-17 11:41:04 +0200

    receptor_tierra

--- EstacioTerra/EstacioTerra.ino
+++ EstacioTerra/EstacioTerra.ino
@@ -3,6 +3,8 @@ SoftwareSerial mySerial(10, 11); // RX, TX (azul, naranja)
 unsigned long nextMillis = 500;
 const int led = 12;
 bool stateLed = LOW;
+bool stateAlarma = LOW;
+const int alarma = 13;
 void setup() {
    pinMode (led, OUTPUT);
    Serial.begin(9600);
@@ -13,8 +15,18 @@ void loop() {
       stateLed = HIGH;
       digitalWrite (led, stateLed);
       String data = mySerial.readString();
+      String paraula = String data.trim();
       Serial.print(data);
       stateLed = LOW;
       digitalWrite (led, stateLed);
+      if (paraula == 'Fallo'){
+         stateAlarma = HIGH;
+         digitalWrite (alarma, stateAlarma);
+      }
+      else{
+         stateAlarma = LOW;
+         digitalWrite (alarma, stateAlarma);
+      }
+
    }
 }
--- prova.py
+++ prova.py
@@ -41,6 +41,8 @@ def comunicacion():
 
 def INICIAR ():
     print ('Has pulsado el botón INICIAR')
+    command = comunicacion
+
 def PAUSAR ():
     print ('Has pulsado el botón PAUSAR')
     mensaje = "Parar"
@@ -65,7 +67,7 @@ def REANUDAR ():
     button_funcions_frame.rowconfigure(2, weight=1)
     button_funcions_frame.columnconfigure(0, weight=1)
 
-    button1 = tk.Button(button_funcions_frame, text="INICIAR", command=INICIAR) # FALTA DEFINIR SHOW_DATA (MOSTRAR DADES)
+    button1 = tk.Button(button_funcions_frame, text="INICIAR", command=comunicacion) # FALTA DEFINIR SHOW_DATA (MOSTRAR DADES)
     button1.grid(row=0, column=0, padx=5, pady=5, sticky=tk.N + tk.E + tk.W + tk.S)
 
     button2 = tk.Button(button_funcions_frame, text="INICIAR", command=PAUSAR) # FALTA DEFINIR NOT_SHOW_DATA (NO MOSTRAR DADES)
