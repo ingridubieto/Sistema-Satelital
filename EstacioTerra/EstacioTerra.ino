7354342eccd4245e9b9350a07de6f87715798e63
LuciaRodriguezCanals <lucia.rodriguez.canals@estudiantat.upc.edu>
2025-11-14 09:05:37 +0100

    Estació Terra Funcions

--- EstacioTerra/EstacioTerra.ino
+++ EstacioTerra/EstacioTerra.ino
@@ -1,41 +1,96 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
+#include <NewPing.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX (azul, naranja)
+SoftwareSerial mySerial(10, 11); // RX, TX (blau, taronja)
 
-unsigned long nextMillis = 500;
+// Definició LED recepció
 const int led = 12;
 bool stateLed = LOW;
+
+// Definició Alarma
 const int alarma = 13;
 bool stateAlarma = LOW;
+
+// Definició servo motor
+Servo servo;
+int pinServo = 9;
+
+// Definició sensor Ultrasons
+const int UltrasonicPin = 5;
+const int MaxDistance = 200;
+const int alarmaSensor = 7;
+
+// Inicialització del sensor ultrasons
+NewPing sonar(UltrasonicPin, UltrasonicPin, MaxDistance);
+
+char missatge;
+//int pinJoystick = A0;
+long nextMillisSENSOR = 1000;
+
+
+
+//int llegirJoystick() {
+  //int valor = analogRead(pinJoystick); // 0-1023
+  //int angle = map(valor, 0, 1023, 0, 180); // Convertim a graus
+  //return angle; // Retornem el resultat
+//}
+
+void deteccioError (){
+   String data = mySerial.readString();
+   //Serial.print(data);
+   data.trim();
+   if (data == "Fallo"){
+      stateAlarma = HIGH;
+   }
+   else{
+         stateAlarma = LOW;
+         }
+   digitalWrite(alarma, stateAlarma);
+}
+
+int obtenirDistancia() {
+  int distancia = sonar.ping_cm();
+  return distancia;
+}
+
+void mostrarDistancia(int distancia) {
+  if (distancia == 0){
+    Serial.println("Fuera de rango");
+    digitalWrite(alarmaSensor, HIGH);
+    delay(500);
+    digitalWrite(alarmaSensor, LOW);
+    delay(500);
+  }
+  else{
+    Serial.print(distancia);
+    Serial.println(" cm");
+  }
+}
 
 void setup() {
    pinMode (led, OUTPUT);
    pinMode (alarma, OUTPUT);
+   pinMode (alarmaSensor, OUTPUT);
+   servo.attach(pinServo);
 
    Serial.begin(9600);
    mySerial.begin(9600);
+
+   nextMillisSENSOR = millis() + 1000;
 }
 
 void loop() {
    if (mySerial.available()) {
-      stateLed = HIGH;
-      digitalWrite (led, stateLed);
-      String data = mySerial.readString();
-      Serial.print(data);
-      stateLed = LOW;
-      digitalWrite (led, stateLed);
-      data.trim();
-      if (data == "Fallo"){
-         stateAlarma = HIGH;
+      encendreLeds();
+      deteccioError();
+      int angle = llegirJoystick(); // cridem funció
+      moureServo(angle);            // cridem funció
+      delay(100);
+      if (millis() >= nextMillisSENSOR){
+         int distancia = obtenirDistancia();
+         mostrarDistancia(distancia);
+         nextMillisSENSOR = millis() + 1000;
       }
-      else{
-         stateAlarma = LOW;
-      }
-      digitalWrite (alarma, stateAlarma);
-   }
-   if (Serial.available()) { // Enviar tipo de función que debe realizar el satélite
-      String comando = Serial.readString();
-      comando.trim();
-      mySerial.println(comando);
    }
 }
